6b2e8ab395335b94b799758904ccb4589ee4c4ca
aburger <aburger277@gmail.com>
2025-01-18 14:06:09 +0900

    리액트에서 html 레이아웃 만들 때 쓰는 JSX 문법 3개(className, {Data Binding}, style

--- blog/src/App.jsx
+++ blog/src/App.jsx
@@ -1,35 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
+// JS 객체로서 HTML을 생성하여 로드하는 메커니즘
 function App() {
-  const [count, setCount] = useState(0)
+  // Binding용 데이터
+  let post = '마포 라멘 맛집';
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    // react 문법으로 Html Class 작성할 때 className이라는 property를 사용해야한다.
+    // html문법으로 `class`라고 적게 될 시 JS Class로 컴파일 됨
+    <div className='App'>
+      <div className='black-nav'>
+        {/* style적용도 {} 사용해서 할 것
+          Json형식인듯
+          style속성에서 사용되는 '-'hyphen은 사용하지 말것(뺄셈으로 취급됨)
+            => camelCase 사용 */}
+        <h4 style={{color: 'red', fontSize: '16px'}}>This is Blog</h4>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+      {/* {}문법으로  데이터 Binding 가능*/}
+      <h4>{post}</h4>
+    </div>
   )
 }
-
 export default App
